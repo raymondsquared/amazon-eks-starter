95bb667b8efb81be37c5ea1eb1087c44b5d6b967
Ray <raymond_squared@live.com>
2022-05-24 00:11:04 +1000

    feat: helm redis

---
+++ helm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Step 1.
+# helm version
+# helm repo list
+# helm repo update
+
+# Step 2.
+# helm install paw-eks-helm-redis-dev-sydney stable/redis 
+
+# Step 3.
+kubectl get pod
+kubectl get nodes
+# helm ls
+
+# Step 4.
+# helm uninstall paw-eks-helm-redis-dev-sydney 
